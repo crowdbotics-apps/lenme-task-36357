a26477f84d23e780c884a2310212f09f3552e3ee
Crowdbotics <team@crowdbotics.com>
2022-07-22 20:32:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lenme_task_36357",
+  url: "https://lenme_task_36357.botics.co",
+  api: "https://lenme_task_36357.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
